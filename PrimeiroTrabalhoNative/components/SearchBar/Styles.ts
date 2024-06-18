5884d5d5b7e660575511daab2b41f63c49d63564
Mateus Daltro <162245009+RandomlyActing@users.noreply.github.com>
2024-06-18 16:09:59 -0300

    Ajuda atualizado e SearchBar adicionado

---
+++ PrimeiroTrabalhoNative/components/SearchBar/Styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent:'center',
+    },
+    item: {
+      backgroundColor: "Gray",
+      margin: 10,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 20,
+      width: '80%',
+      paddingHorizontal: 10,
+      marginTop: 20,
+      borderRadius: 10,
+    },
+  });
+
+export default styles
