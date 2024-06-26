aca3c543ee374cab0d8f708b1a5fd2e14c75d375
adinor <adinor_original@hotmail.com>
2024-06-26 14:02:28 -0300

    Ajuste no estilo

--- ProjetoFinalReactNative/screens/Sobre/style.ts
+++ ProjetoFinalReactNative/screens/Sobre/style.ts
@@ -1,18 +1,22 @@
 import { StyleSheet } from 'react-native';
 
- export const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: 'gray',
+    padding: 30,
+    
   },
   card: {
     marginVertical: 5,
     elevation: 3,
-    
+    marginTop: 30
   },
   avatar: {
     backgroundColor: '#e0e0e0',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
