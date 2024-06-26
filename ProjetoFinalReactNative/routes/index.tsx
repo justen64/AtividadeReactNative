efd82c0ca9dd96014c773ee74bd0c22818e14580
adinor <adinor_original@hotmail.com>
2024-06-26 13:39:18 -0300

    Contatos p/

--- ProjetoFinalReactNative/routes/index.tsx
+++ ProjetoFinalReactNative/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from '../screens/Login';
-import { ContactsList } from '../components/ContactsList';
+import { ContatosLista } from '../screens/ContatosLista';
 import Sobre from '../screens/Sobre'
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
    
       <Drawer.Navigator>
         <Drawer.Screen name="Login" component={Login} />
-        <Drawer.Screen name="Contatos" component={ContactsList} />
+        <Drawer.Screen name="Contatos" component={ContatosLista} />
         <Drawer.Screen name="Sobre" component={Sobre} />
       </Drawer.Navigator>
     
